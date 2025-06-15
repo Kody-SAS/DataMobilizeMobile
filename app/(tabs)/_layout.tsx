31c6a16070ae7189ef84bab5c29304179c78c7ff
monkeyK1n9 <mkeyk1n9@gmail.com>
2025-06-15 11:52:47 +0200

    implement report fetching on navigation to the map page

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -5,13 +5,25 @@ import { TabBarIcon } from '../../components/navigation/TabBarIcon'
 import { Colors } from '../../constants/Colors';
 import { useColorScheme } from '../../hooks/useColorScheme';
 import { useTranslation } from 'react-i18next';
+import { useDispatch } from 'react-redux';
+import { ThunkDispatch } from '@reduxjs/toolkit';
+import { getAllReports } from '../../redux/slices/homeSlice';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
   const {t} = useTranslation();
+  const dispatch = useDispatch<ThunkDispatch<any, any, any>>();
 
   return (
     <Tabs
+      screenListeners={{
+        tabPress: (e) => {
+          const tab = e.target?.split("-")[0];
+          if (tab == "map") {
+            dispatch(getAllReports()); // fetch the reports every time we navigate to the map tab page
+          }
+        }
+      }}
       screenOptions={{
         tabBarActiveTintColor: Colors.light.icon.primary,
         headerShown: false,
