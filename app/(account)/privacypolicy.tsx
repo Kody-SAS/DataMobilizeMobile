ed2bc392d17734aa07f778b765810cd7fd33ad97
monkeyK1n9 <mkeyk1n9@gmail.com>
2025-06-21 20:09:51 +0200

    fix the privacy policy and automate submit

--- app/(account)/privacypolicy.tsx
+++ app/(account)/privacypolicy.tsx
@@ -1,9 +1,19 @@
+import { Asset } from "expo-asset";
+import { useEffect, useState } from "react";
 import { Platform } from "react-native";
 import Pdf from "react-native-pdf";
 
 export default function PrivacyPolicy() {
-    const pdfSource = Platform.OS == 'ios' ? require("../../assets/docs/privacypolicy.pdf") : {uri: "bundle-assets://privacypolicy.pdf"}
-    
+    const [androidPdf, setAndroidPdf] = useState<string | null>(null);
+    const pdfSource = Platform.OS == 'ios' ? require("../../assets/docs/privacypolicy.pdf") : {uri: androidPdf}
+
+    useEffect(() => {
+        const loadPdf = async () => {
+            const pdf = await Asset.loadAsync(require("../../assets/docs/privacypolicy.pdf"))
+            setAndroidPdf(pdf[0]?.localUri || pdf[0]?.uri);
+        }
+        loadPdf();
+    }, [])
     return (
         <Pdf
             source={pdfSource}
