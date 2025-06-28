10f657a996c2d29f635160a9101adb7d7c58e268
monkeyK1n9 <mkeyk1n9@gmail.com>
2025-06-28 18:57:30 +0200

    update app version

--- app.config.js
+++ app.config.js
@@ -4,7 +4,7 @@ export default {
   expo: {
     name: "Data Mobilize",
     slug: "DataMobilize",
-    version: "1.0.3",
+    version: "1.0.4",
     orientation: "portrait",
     icon: "./assets/images/applogo.png",
     scheme: "myapp",
@@ -23,7 +23,7 @@ export default {
         foregroundImage: "./assets/images/applogo.png",
         backgroundColor: "#ffffff"
       },
-      versionCode: 4,
+      versionCode: 5,
       package: "com.kody.datamobilize",
       config: {
         googleMaps: {
